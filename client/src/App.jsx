71e738fca4c5fca7249c37f8bf068c0b425a517b
Shamikh <93029584+shamikh1014@users.noreply.github.com>
2025-02-01 09:19:11 +0530

    create and add the footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import SignUp from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 export default function App(){
     return(
@@ -24,9 +25,11 @@ export default function App(){
                 <Route path="/sign-up" element={<SignUp/>}/>
                 <Route path="/dashboard" element={<Dashboard/>}/>
                 <Route path="/projects" element={<Projects/>}/>
-
                 
             </Routes>
+
+            <Footer/>
+
         </BrowserRouter>
 
     )
--- client/vite.config.js
+++ client/vite.config.js
@@ -7,12 +7,13 @@ export default defineConfig({
     server:{
         proxy:{
           '/api':{
+
               target:'http://localhost:3000',
-              changeOrigin:true,
               secure:false,
 
             },
         },
     },
+    
     plugins: [react()],
 })
