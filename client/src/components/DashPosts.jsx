24cc96542e9ac8bc2a9d3ebf47c4955fd48c123b
Shamikh <93029584+shamikh1014@users.noreply.github.com>
2025-02-05 09:55:50 +0530

    add show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -11,6 +11,7 @@ export default function DashPosts(){
 
     const {currentUser}=useSelector((state)=>state.user);
     const [userPosts, setUserPosts]=useState([]);
+    const [showMore, setShowMore]=useState(true);
 
     useEffect(()=>{
 
@@ -22,6 +23,10 @@ export default function DashPosts(){
 
                 if(res.ok){
                     setUserPosts(data.posts);
+                    if(data.posts.length<9){
+                        setShowMore(false);
+                    }
+
                 }
 
             }
@@ -37,8 +42,30 @@ export default function DashPosts(){
 
     }, [currentUser._id]);
 
+    const handleShowMore=async()=>{
+        const startIndex=userPosts.length;
+
+        try{
+
+            const res=await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`);
+            const data=await res.json();
+
+            if(res.ok){
+                setUserPosts((prev)=>[...prev, ...data.posts]);
+                if(data.posts.length<9){
+                    setShowMore(false);
+                }
+            }
+
+        }
+        catch(error){
+            console.log(error.message);
+        }
+
+    }
+
     return(
-        
+
         <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
             {currentUser.isAdmin && userPosts.length>0?(
 
@@ -102,6 +129,14 @@ export default function DashPosts(){
                         ))}
 
                     </Table>
+
+                    {
+                        showMore && (
+                            <button onClick={handleShowMore} className='w-full text-teal-500 self-center text-sm py-7'>
+                                Show more
+                            </button>
+                        )
+                    }
                 </>
 
             ):(
