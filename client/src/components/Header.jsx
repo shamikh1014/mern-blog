f614aacc2d58db9a907b24a7757d796822701bf8
Shamikh <93029584+shamikh1014@users.noreply.github.com>
2025-02-09 22:57:37 +0530

    complete search page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,21 +2,40 @@
 // import React from 'react'
 
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon, FaSun } from "react-icons/fa";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleTheme } from '../redux/theme/themeSlice.js';
 import { signOutSuccess } from "../redux/user/userSlice.js";
+import { useEffect, useState } from "react";
 
 
 export default function Header(){
 
     const path=useLocation().pathname;
+
+    const location=useLocation();
+    const navigate=useNavigate();
+
     const dispatch=useDispatch();
 
     const {currentUser}=useSelector(state=>state.user);
     const {theme}=useSelector((state)=>state.theme);
+
+    const [searchTerm, setSearchTerm]=useState('');
+
+
+    useEffect(()=>{
+
+        const urlParams=new URLSearchParams(location.search);
+        const searchTermFromUrl=urlParams.get('searchTerm');
+
+        if(searchTermFromUrl){
+            setSearchTerm(searchTermFromUrl);
+        }
+
+    }, [location.search]);
 
     const handleSignout=async()=>{
     
@@ -40,6 +59,18 @@ export default function Header(){
     
         };
 
+        const handleSubmit=(e)=>{
+
+            e.preventDefault();
+
+            const urlParams=new URLSearchParams(location.search);
+            urlParams.set('searchTerm', searchTerm);
+
+            const searchQuery=urlParams.toString();
+            navigate(`/search?${searchQuery}`);
+
+        }
+
     return(
         <Navbar className='border-b-2'>
 
@@ -52,7 +83,7 @@ export default function Header(){
 
             </Link>
 
-            <form>
+            <form onSubmit={handleSubmit}>
 
                 <TextInput
 
@@ -60,6 +91,8 @@ export default function Header(){
                     placeholder='Search...'
                     rightIcon={AiOutlineSearch}
                     className='hidden lg:inline'
+                    value={searchTerm}
+                    onChange={(e)=>setSearchTerm(e.target.value)}
 
                 />
 
